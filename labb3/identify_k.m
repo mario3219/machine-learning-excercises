d3e97d1f34379c3757848139d9dd530c4ea856dc
Jonathan <jonathan.olsson8@gmail.com>
2024-12-10 16:10:23 +0100

    börja med labb 3

---
+++ labb3/identify_k.m
@@ -0,0 +1,35 @@
+run('init_quadcopter_model')
+run('init_quadcopter_states')
+run('init_noise_levels')
+addpath ('models')
+%%
+Omega_in.time = (0:inner_h:10)';
+Omega_in.signals.values = ones(length(Omega_in.time),1)*ones(1,4); %You will need to change ones to something better, HINT linspace
+%%
+disp('starting sim')
+out = sim('omega_input','StopTime', '5')
+disp('sim done')
+%%
+figure(1)
+clf
+plot(out.p)
+title('Position')
+legend('x','y','z')
+%% Using Toolbox, estimating from accelerometer.
+%dat1 = TODO;
+dat2 = iddata(out.acc.data(:,3)+g,4*out.Omega.data(:,1).^2,inner_h);
+%dat3 = TODO;
+%sys1 =TODO
+sys2 = procest(dat2,'p0') %p0 means zero poles
+%sys3 =TODO
+figure(2)
+clf
+subplot(3,1,1)
+compare(dat1,sys1)
+subplot(3,1,2)
+compare(dat2,sys2)
+subplot(3,1,3)
+compare(dat3,sys3)
+
+%k_est = TODO;
+k
---
+++ labb3/identify_wind.m
@@ -0,0 +1,52 @@
+run('init_quadcopter_model')
+run('init_quadcopter_states_2')
+addpath ('models')
+run('initialize_wind_dist')
+run('design_pid')
+y_ref = 0
+
+%%
+disp('starting sim')
+out = sim('closed_loop','StopTime', '50')
+disp('done sim')
+%% From wind
+R = 1;%TODO might be needed to change
+y_acc = out.acc.data(:,2);
+Ft = out.T.data.*sin(out.eta.data(:,1));
+wind_force_est = m*y_acc-Ft;
+dat = iddata(wind_force_est(1:R:end),[],sample_time*R);
+opts = armaxOptions;
+armax2 = armax(dat,[]) %TODO select suitable model structure
+figure(1)
+clf
+compare(dat,armax2,3)
+
+figure(2)
+clf
+wnyq = pi*1/sample_time/R;
+wvec = logspace(-1,log10(wnyq),10000);
+p = bodeoptions;
+set(p,'MagUnits','abs','MagScale','log')
+bode(Gwindd*wind_power^(1/2)/(sqrt(2*wnyq)),wvec,'b',p);
+grid on
+hold on
+
+Harmax2 = tf(armax2.C,armax2.A,armax2.Ts);
+sigma2 = armax2.NoiseVariance;
+bode(Harmax2*sqrt(sigma2),'r');
+fig2 = gcf;
+ax = findall (fig2, 'type', 'axes');
+set(ax(3),'YLim', [1e-4 1e2])
+legend('reference', 'estimate')
+
+%% Converting to Continuous time
+wind_est = d2c(Harmax2)
+wind_denum = wind.denum
+%% Plotting - Inspect position.
+figure(3)
+clf
+plot(out.p)
+title('Position')
+legend('x','y','z')
+
+
